ff6b30dc892aa2b451bda773d2948364f7de0b09
Mirjam Kaufmann <kaufm5@bfh.ch>
2018-11-10 11:06:29 +0100

    Grey bar and date

--- app/src/main/java/ch/bfh/btx8108/trinkster/Statistic.java
+++ app/src/main/java/ch/bfh/btx8108/trinkster/Statistic.java
@@ -1,17 +1,38 @@
 package ch.bfh.btx8108.trinkster;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.ArrayList;
+import java.util.SimpleTimeZone;
+import java.util.Locale;
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabItem;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.TabItem;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.graphics.Color;
+import android.app.Dialog;
+import android.app.DatePickerDialog;
+import android.view.View.OnClickListener;
 
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.data.Entry;
-import java.util.ArrayList;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.formatter.PercentFormatter;
@@ -21,15 +42,77 @@ import com.github.mikephil.charting.components.Legend.LegendPosition;
 
 public class Statistic extends Fragment {
 
+    Calendar myCalendar = Calendar.getInstance();
+    String date;
+    String actualDay;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.activity_statistic, container, false);
 
-        TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-        textView.setText( "STATISTIC-SCREEN" );
+        final TextView textViewDate = (TextView) rootView.findViewById(R.id.dateView);
+        setDate(textViewDate);
+
+        final ImageButton buttonCalendar = rootView.findViewById(R.id.calendar);
+        buttonCalendar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                showCalendar();
+                changeDate(textViewDate);
+            }
+        });
+
+        final ImageButton buttonBefore = rootView.findViewById(R.id.dayBefore);
+        buttonBefore.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+
+            }
+        });
+
+        final ImageButton buttonAfter = rootView.findViewById(R.id.dayAfter);
+        buttonAfter.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+
+            }
+        });
+
+        final ImageButton buttonTimeline = rootView.findViewById(R.id.timeline);
+        buttonTimeline.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+
+            }
+        });
 
         PieChart pieChart = (PieChart) rootView.findViewById(R.id.piechart);
+        setPieChart(pieChart);
+
+
+
+
+        return rootView;
+    }
+
+    public void showCalendar() {
+
+    }
+
+    public void changeDate(TextView item) {
+        item.setText(date);
+    }
+
+    public void setDate (TextView item){
+        Date today = Calendar.getInstance().getTime();//getting date
+        SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy");//formating according to my need
+        date = formatter.format(today);
+        actualDay = formatter.format(today);
+        item.setText(date);
+    }
+
+    public void setPieChart (PieChart pieChart) {
         pieChart.setUsePercentValues(true);
 
         // IMPORTANT: In a PieChart, no values (Entry) should have the same
@@ -74,12 +157,13 @@ public class Statistic extends Fragment {
 
         //legend
         Legend l = pieChart.getLegend();
-        l.setPosition(LegendPosition.RIGHT_OF_CHART);
+        l.setPosition(LegendPosition.RIGHT_OF_CHART_CENTER);
         l.setXEntrySpace(7);
         l.setYEntrySpace(5);
 
         pieChart.invalidate(); // refresh
-
-        return rootView;
     }
+
+
+
 }
